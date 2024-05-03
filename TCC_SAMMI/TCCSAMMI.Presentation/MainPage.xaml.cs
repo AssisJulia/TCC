f14ba3fe0a600afaec0ff01637ac723061412c1e
MiguelOliveira1301 <miguelclasher0192@gmail.com>
2024-05-02 22:21:34 -0300

    migração para o hibrido(fazeroq)

---
+++ TCC_SAMMI/TCCSAMMI.Presentation/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TCCSAMMI.Presentation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
