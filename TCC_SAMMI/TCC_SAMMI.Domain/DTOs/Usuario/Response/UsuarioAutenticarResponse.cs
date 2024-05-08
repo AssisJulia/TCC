456a8905d59fac201cfa26ba351acf5232b4589b
Meuamigo1301 <miguelclasher0192@gmail.com>
2024-05-07 22:48:57 -0300

    Tentativa de integrar a interface com a api

---
+++ TCC_SAMMI/TCC_SAMMI.Domain/DTOs/Usuario/Response/UsuarioAutenticarResponse.cs
@@ -0,0 +1,20 @@
+﻿
+namespace TCC_SAMMI.Domain.DTOs.Usuario.Response;
+
+
+public class UsuarioAutenticarResponse
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string AccessToken { get; set; }
+
+    public UsuarioAutenticarResponse(
+        Guid id,
+        string nome,
+        string accessToken)
+    {
+        Id = id;
+        Nome = nome;
+        AccessToken = accessToken;
+    }
+}
